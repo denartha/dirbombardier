60cd6bb4100902537a012136e3717fe11184c845
denartha@gmail.com <denartha@gmail.com>
2017-05-21 14:03:58 +0100

    Stopped it printing the directories it didnt find, as it just spams yours screen.

--- dirbombadier.py
+++ dirbombadier.py
@@ -5,7 +5,7 @@ import sys
 import argparse
 
 
-ap = argparse.ArgumentParser()
+ap = argparse.ArgumentParser(description='dirbombardier brute force directories on a webserver or application. Simply supply a URL and a wordlist', usage='dirbombardier http://site.org/ wordlist.txt'   )
 group = ap.add_mutually_exclusive_group(required=True)
 group.add_argument('-u', nargs=2) # specify url to brute
 
@@ -16,9 +16,15 @@ url = opts.u[0]
 word_list = opts.u[1]
 #word_list = 'small.txt'
 
+
+
 directories  = open(word_list, 'r')
 dirs = directories.readlines()
+lines = len(dirs)
 directories.close()
+
+
+print "Starting bruteforce of " + url + " with " + str(lines) + " directories"
 
 for dir in dirs:
 	uri = url + "/" + dir
@@ -28,9 +34,9 @@ for dir in dirs:
 		d = r.status_code
 		code = '200'
 		if d == int(code):
-			print "URL: " + uri.strip()
+			print "URL Found: " + uri.strip()
 		else:
-			print "Not found: " + uri.strip()
+			pass
 
 	except requests.ConnectionError, e:
     		print e 
